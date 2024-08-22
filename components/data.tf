aac9039cbdf41fb4a51167b79abcea9ceb0c48ed
Connor O'Kane <connor.o'kane@justice.gov.uk>
2024-08-22 13:24:42 +0100

    adding test team from hmcts-test

--- components/data.tf
+++ components/data.tf
@@ -1,5 +1,5 @@
 data "github_team" "admin" {
-  slug = "" # Add more teams here if you want to exlcude them from the rulesets
+  slug = "test" # Add more teams here if you want to exlcude them from the rulesets
 }
 
 data "local_file" "repos_json" {
